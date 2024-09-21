88b3a0bf9d38e89c1725c1890b9a4f149a1f2d1c
82everywin <82everywin@gmail.com>
2024-09-22 07:49:37 +0900

    env: JPA 설정 코드

---
+++ src/main/java/com/study/backend/blog/domain/Article.java
@@ -0,0 +1,22 @@
+package com.study.backend.blog.domain;
+
+import com.study.backend.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+}
